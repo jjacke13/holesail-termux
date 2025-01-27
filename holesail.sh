e796380870eecc3abcbdac8805c060f8e7ce93b4
Vaios K <jjacke@proton.me>
2025-01-27 10:20:27 +0200

    Update holesail.sh

--- holesail.sh
+++ holesail.sh
@@ -10,9 +10,23 @@ rm ./holesail/package.json
 cat << EOF > ./holesail/package.json
 {
   "name": "holesail",
-  "version": "1.8.2",
+  "version": "1.10.0",
   "description": "Holesail is TCP/UDP peer-to-peer proxy. Holesail let's you instantly share any application running on a specific port from your local computer.",
   "main": "index.js",
+  "pear": {
+    "name": "holesail",
+    "type": "terminal"
+  },
+  "imports": {
+    "util": {
+      "bare": "bare-utils",
+      "default": "util"
+    },
+    "fs": {
+      "bare": "bare-fs",
+      "default": "fs"
+    }
+  },
   "scripts": {
     "test": "npm test",
     "refresh": "rm package-lock.json && rm -rf node_modules && npm i && npm update"
@@ -32,25 +46,56 @@ cat << EOF > ./holesail/package.json
     "url": "https://github.com/holesail/holesail/issues"
   },
   "homepage": "https://github.com/holesail/holesail#readme",
+  "pkg": {
+    "assets": [
+      "includes/**/*"
+    ]
+  },
   "dependencies": {
     "b4a": "^1.6.6",
+    "bare-crypto": "^1.3.1",
+    "bare-events": "^2.5.0",
+    "bare-fs": "^4.0.1",
+    "bare-http1": "^4.0.2",
+    "bare-net": "^2.0.0",
+    "bare-os": "^3.3.0",
+    "bare-path": "^3.0.0",
+    "bare-process": "^4.0.0",
+    "bare-querystring": "^1.0.0",
+    "bare-subprocess": "^5.0.2",
+    "bare-tty": "^5.0.1",
+    "bare-utils": "1.1.1",
+    "barely-pm2": "^0.0.4",
+    "child_process": "npm:bare-subprocess@^5.0.2",
     "cli-box": "^6.0.10",
     "colors": "^1.4.0",
+    "crypto": "npm:bare-crypto@^1.3.1",
+    "events": "npm:bare-events@^2.5.0",
+    "fs": "npm:bare-node-fs@^1.0.2",
     "graceful-goodbye": "^1.3.0",
     "holesail-client": "^1.1.6",
-    "holesail-server": "^1.4.2",
+    "holesail-server": "^1.4.4",
+    "http": "npm:bare-http1@^4.0.2",
     "hyper-cmd-lib-keys": "^0.1.1",
     "hyperdht": "^6.20.0",
     "minimist": "^1.2.8",
-    "pm2": "^5.4.2",
+    "net": "npm:bare-net@^2.0.0",
+    "os": "npm:bare-os@^3.3.0",
+    "path": "npm:bare-path@^3.0.0",
+    "process": "npm:bare-process@^4.0.0",
     "qrcode-terminal": "^0.12.0",
-    "standard": "^17.1.2",
-    "udx-native": "1.11.2", 
+    "querystring": "npm:bare-querystring@^1.0.0",
+    "tty": "npm:bare-tty@^5.0.1",
+    "util": "npm:bare-utils@^1.1.1",
+    "which-runtime": "^1.2.1",
+    "udx-native": "1.11.2",
     "sodium-native": "4.1.1"
   },
   "bin": {
-    "holesail": "./index.js",
-    "holesail-manager": "./manager.js"
+    "holesail": "./index.js"
+  },
+  "devDependencies": {
+    "standard": "^17.1.2"
   }
 }
 EOF
